7219a7fe4a311c82aa91d967e29913cdedd90270
Luiz Felipe K <luizfelipekirchbaner@gmail.com>
2021-07-28 16:26:05 -0300

    feat added flex center

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,80 +1,82 @@
 <template>
-  <q-page class="bg-indigo-1">
-    <q-card class="my-card fit custom-shadow q-pb-sm bg-grey-11" bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar size="50px">
-            <img src="../assets/avatar.png">
-          </q-avatar>
-        </q-item-section>
+  <q-page class="bg-indigo-1 flex flex-center">
+    <div style="max-width: 400px;">
+      <q-card class="my-card fit custom-shadow q-pb-sm bg-grey-11" bordered>
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar size="50px">
+              <img src="../assets/avatar.png">
+            </q-avatar>
+          </q-item-section>
 
-        <q-item-section>
-          <q-item-label
-            class="text-h6"
-          >
-            Luiz Felipe Kirchbaner
-          </q-item-label>
-          <q-item-label class="text-body1 text-grey-7">
-            Mussum Ipsum, cacilds vidis litro abertis. Paisis, filhis, espiritis santis.
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-card>
-    <div class="text-body1 q-mt-xl text-center text-grey-10">
-      <q-icon name="touch_app" style="font-size: 1.6rem;" />
-      Toque para interagir
-    </div>
-    <div class="q-pa-md">
-      <q-list>
-        <q-item clickable v-ripple class="custom-shadow bg-grey-1 rounded-borders q-mb-lg">
           <q-item-section>
-            <q-avatar
-              size="60px"
-              rounded
-              color="deep-purple-13"
-              text-color="white"
-              icon="fab fa-github"
-            />
+            <q-item-label
+              class="text-h6"
+            >
+              Luiz Felipe Kirchbaner
+            </q-item-label>
+            <q-item-label class="text-body1 text-grey-7">
+              Mussum Ipsum, cacilds vidis litro abertis. Paisis, filhis, espiritis santis.
+            </q-item-label>
           </q-item-section>
-          <q-item-section class="text-h5">GitHub</q-item-section>
         </q-item>
-        <q-item clickable v-ripple class="custom-shadow bg-grey-1 rounded-borders q-mb-lg">
-          <q-item-section>
-            <q-avatar
-              size="60px"
-              rounded
-              color="indigo-1"
-              text-color="deep-purple-13"
-              icon="fab fa-discord"
-            />
-          </q-item-section>
-          <q-item-section class="text-h5">Discord</q-item-section>
-        </q-item>
-        <q-item clickable v-ripple class="custom-shadow bg-grey-1 rounded-borders q-mb-lg">
-          <q-item-section>
-            <q-avatar
-              size="60px"
-              rounded
-              color="deep-purple-13"
-              text-color="white"
-              icon="fab fa-linkedin-in"
-            />
-          </q-item-section>
-          <q-item-section class="text-h5">Linkedin</q-item-section>
-        </q-item>
-        <q-item clickable v-ripple class="custom-shadow bg-grey-1 rounded-borders q-mb-lg">
-          <q-item-section>
-            <q-avatar
-              size="60px"
-              rounded
-              color="indigo-1"
-              text-color="deep-purple-13"
-              icon="fab fa-instagram"
-            />
-          </q-item-section>
-          <q-item-section class="text-h5">Instagram</q-item-section>
-        </q-item>
-      </q-list>
+      </q-card>
+      <div class="text-body1 q-mt-xl text-center text-grey-10">
+        <q-icon name="touch_app" style="font-size: 1.6rem;" />
+        Toque para interagir
+      </div>
+      <div class="q-mt-md">
+        <q-list>
+          <q-item clickable v-ripple class="custom-shadow bg-grey-1 rounded-borders q-mb-lg">
+            <q-item-section>
+              <q-avatar
+                size="60px"
+                rounded
+                color="deep-purple-13"
+                text-color="white"
+                icon="fab fa-github"
+              />
+            </q-item-section>
+            <q-item-section class="text-h5">GitHub</q-item-section>
+          </q-item>
+          <q-item clickable v-ripple class="custom-shadow bg-grey-1 rounded-borders q-mb-lg">
+            <q-item-section>
+              <q-avatar
+                size="60px"
+                rounded
+                color="indigo-1"
+                text-color="deep-purple-13"
+                icon="fab fa-discord"
+              />
+            </q-item-section>
+            <q-item-section class="text-h5">Discord</q-item-section>
+          </q-item>
+          <q-item clickable v-ripple class="custom-shadow bg-grey-1 rounded-borders q-mb-lg">
+            <q-item-section>
+              <q-avatar
+                size="60px"
+                rounded
+                color="deep-purple-13"
+                text-color="white"
+                icon="fab fa-linkedin-in"
+              />
+            </q-item-section>
+            <q-item-section class="text-h5">Linkedin</q-item-section>
+          </q-item>
+          <q-item clickable v-ripple class="custom-shadow bg-grey-1 rounded-borders q-mb-lg">
+            <q-item-section>
+              <q-avatar
+                size="60px"
+                rounded
+                color="indigo-1"
+                text-color="deep-purple-13"
+                icon="fab fa-instagram"
+              />
+            </q-item-section>
+            <q-item-section class="text-h5">Instagram</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
     </div>
   </q-page>
 </template>
